7e0351a08b40bfadbaa23b759157c2b43fd28a02
Matthew Slipper <me@matthewslipper.com>
2019-02-09 12:43:55 -0800

    Make disclaimer more prominent

--- frontend/src/components/Disclaimer.tsx
+++ frontend/src/components/Disclaimer.tsx
@@ -3,6 +3,7 @@ import bemify from '../utils/bemify';
 import {Col, Grid} from './Grid';
 import {ContentArea} from './ContentArea';
 import './Disclaimer.scss';
+import ContentHeader from './ContentHeader';
 
 const b = bemify('disclaimer');
 
@@ -13,8 +14,9 @@ export default class Disclaimer extends React.Component {
         <ContentArea>
           <Grid>
             <Col transparent>
+              <ContentHeader title="Disclaimer" />
               <div className={b('text')}>
-                Disclaimer: Filecoin network and token data included on this dashboard are notional and for test and development purposes only.
+                Filecoin network and token data included on this dashboard are notional and for test and development purposes only.
               </div>
             </Col>
           </Grid>
