650f9649217362dbf8ea505fa900a1033d6ae6c4
Matthew Slipper <me@matthewslipper.com>
2019-02-27 01:48:53 -0800

    Add heapdump

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,10 @@
 import {fromEnv} from './Config';
 import Backend from './Backend';
+import heapdump = require('heapdump');
+
+heapdump.writeSnapshot((err, filename) => {
+  console.log('Heap dump written to:', filename)
+});
 
 const config = fromEnv();
 const backend = new Backend(config);
