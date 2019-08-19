1c6c4355468fd5ff8691e1b0df63f8672f3e0dc4
Matthew Slipper <me@matthewslipper.com>
2019-08-19 10:31:16 -0700

    Fix broken migration rename

--- backend/migrations/20190608002923-update-ask-expiry-len-type.js
+++ backend/migrations/20190608002923-update-ask-expiry-len-type.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20190608002923-update-trigger-param-type-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20190608002923-update-ask-expiry-len-type-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20190608002923-update-trigger-param-type-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20190608002923-update-ask-expiry-len-type-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
